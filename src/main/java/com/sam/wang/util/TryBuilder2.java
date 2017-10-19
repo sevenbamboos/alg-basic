e138bd88f82bca9dcab6ffc3d972a7b838a46b16
Sam Wang <sevenbamboos@gmail.com>
2017-10-19 10:40:53 +0800

    Simple yield for TryBuilder2

--- src/main/java/com/sam/wang/util/TryBuilder2.java
+++ src/main/java/com/sam/wang/util/TryBuilder2.java
@@ -19,7 +19,8 @@ public final class TryBuilder2<R1,R2> {
         this.b2 = b2;
     }
 
-    //TODO What about f can throw exception
+    //TODO What about f can throw exception ?
+    // But map has already taken care of try ?
     public <T> Try<T> yield(BiFunction<R1, R2, T> f) {
         return
             Try.tryWith(b1).flatMap(v1 ->
@@ -27,5 +28,9 @@ public final class TryBuilder2<R1,R2> {
                     f.apply(v1, v2)
                 )
             );
+    }
+
+    public Try<R2> yield() {
+        return yield((b1, b2) -> b2);
     }
 }
