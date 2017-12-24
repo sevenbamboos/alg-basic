0ab934032a72231a6dace53c057aa76e4510c566
sevenbamboos <sevenbamboos@gmail.com>
2017-12-24 20:17:36 +0800

    Property accessor

--- src/main/java/com/samwang/util/Utility.java
+++ src/main/java/com/samwang/util/Utility.java
@@ -2,7 +2,10 @@ package com.samwang.util;
 
 import java.util.Optional;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
+
+import static com.samwang.util.Utility.*;
 
 public enum Utility {
 	;
@@ -21,16 +24,135 @@ public enum Utility {
         }
     }
 
-    public static void foo(Provider<String> s) {
-        System.out.println(s.provide());
+    public static <R,T1> Optional<R> get(T1 root, Function<T1,R> prop1) {
+        return root == null
+            ? Optional.empty()
+            : Optional.ofNullable(prop1.apply(root));
     }
 
-    @FunctionalInterface
-    private interface Provider<T> {
-        T provide();
+    public static <R,T1,T2> Optional<R> get(T1 root, Function<T1,T2> prop1, Function<T2,R> prop2) {
+        return get(root, prop1).map(t -> prop2.apply(t));
+    }
+
+    public static <R,T1,T2,T3> Optional<R> get(T1 root, Function<T1,T2> prop1, Function<T2,T3> prop2, Function<T3,R> prop3) {
+        return get(root, prop1, prop2).map(t -> prop3.apply(t));
+    }
+
+    public static <R,T1,T2,T3,T4> Optional<R> get(T1 root, Function<T1,T2> prop1, Function<T2,T3> prop2, Function<T3,T4> prop3, Function<T4,R> prop4) {
+        return get(root, prop1, prop2, prop3).map(t -> prop4.apply(t));
+    }
+
+    static class Student {
+        Teacher teacher;
+
+        public Teacher getTeacher() {
+            return teacher;
+        }
+
+        public void setTeacher(Teacher teacher) {
+            this.teacher = teacher;
+        }
+    }
+
+    static class Teacher {
+        Email email;
+
+        public Email getEmail() {
+            return email;
+        }
+
+        public void setEmail(Email email) {
+            this.email = email;
+        }
+    }
+
+    static class Email {
+        String address;
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+    }
+
+    private static String teacherEmailWithEarlyBreak(Student student) {
+
+        if (student == null) return null;
+        if (student.getTeacher() == null) return null;
+        if (student.getTeacher().getEmail() == null) return null;
+
+        return student.getTeacher().getEmail().getAddress();
+    }
+
+    private static String teacherEmailWithIf(Student student) {
+
+        if (student != null
+            && student.getTeacher() != null
+            && student.getTeacher().getEmail() != null) {
+
+            return student.getTeacher().getEmail().getAddress();
+
+        } else {
+            return null;
+        }
+    }
+
+    private static String teacherEmailWithPropertyAccessor(Student student) {
+
+        return get(student,
+            Student::getTeacher,
+            Teacher::getEmail,
+            Email::getAddress
+
+        ).orElse(null);
+    }
+
+    private static Optional<String> teacherEmail(Student student) {
+        return get(student,
+            Student::getTeacher,
+            Teacher::getEmail,
+            Email::getAddress);
+    }
+    
+    private static void changeEmail(Student student, Function<Student, String> emailRetriever) {
+        String oldValue = emailRetriever.apply(student);
+        if (oldValue != null) {
+            System.out.println("Old value:" + oldValue);
+            System.out.println("New value:" + oldValue.replaceFirst("qq", "163"));
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-        foo(() -> "abc");
+
+        String oldMail = "icanfly@qq.com";
+
+        Email aEmail = new Email();
+        aEmail.setAddress(oldMail);
+
+        Teacher aTeacher = new Teacher();
+        aTeacher.setEmail(aEmail);
+
+        Student aStudent = new Student();
+        aStudent.setTeacher(aTeacher);
+
+        System.out.println("teacherEmailWithEarlyBreak");
+        changeEmail(aStudent, Utility::teacherEmailWithEarlyBreak);
+
+        System.out.println("teacherEmailWithIf");
+        changeEmail(aStudent, Utility::teacherEmailWithIf);
+
+        System.out.println("teacherEmailWithPropertyAccessor");
+        changeEmail(aStudent, Utility::teacherEmailWithPropertyAccessor);
+
+        System.out.println("teacherEmail");
+        teacherEmail(aStudent)
+            .map(s -> s.replaceFirst("qq", "163"))
+            .map(s -> "New value:" + s)
+            .ifPresent(System.out::println);
     }
+
 }
