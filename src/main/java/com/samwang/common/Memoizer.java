e9512880f38722cad29bf55001d59f66c7c9d292
Yuping Wang <sevenbamboos@gmail.com>
2017-12-26 15:09:00 +0800

    Update Memoizer.java

--- src/main/java/com/samwang/common/Memoizer.java
+++ src/main/java/com/samwang/common/Memoizer.java
@@ -3,18 +3,46 @@ package com.samwang.common;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+// Memorize computational result for Function or Supplier 
 public class Memoizer<T, U> {
 
+    // major for test purpose to verify actual count of execution
+    public static interface MemoizerCounter {
+    	int executed();
+    	int performed();
+    }    
+    
     private final Map<T, U> cache = new ConcurrentHashMap<>();
 
-    private Memoizer() {
-    }
+	private int executedCount = 0;
+    private int performedCount = 0;    
+    
+    private Memoizer() {}
 
     public static <T, U> Function<T, U> memoize(Function<T, U> function) {
         return new Memoizer<T, U>().doMemoize(function);
     }
 
-    private Function<T, U> doMemoize(Function<T, U> function) {
+    public Function<T, U> doMemoize(Function<T, U> function) {
         return input -> cache.computeIfAbsent(input, function::apply);
     }
+    
+	public Supplier<U> doMemoize(T key, Supplier<U> supplier) {
+	  	return toSupplier(key, doMemoize(toFunc(supplier)));
+	}
+	    
+	private Function<T, U> toFunc(Supplier<U> supplier) {
+	   	return _ignored -> supplier.get();
+	}
+	    
+    private Supplier<U> toSupplier(T t, Function<T, U> func) {
+	    return () -> func.apply(t);
+	}    
+    
+    public MemoizerCounter counter() {
+	    return new MemoizerCounter() {
+			@Override public int executed() { return executedCount; }
+			@Override public int performed() { return performedCount; }
+	    };
+    }    
 }
